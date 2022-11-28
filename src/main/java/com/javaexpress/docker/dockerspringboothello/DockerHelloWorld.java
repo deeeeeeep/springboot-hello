dd10133fe465baab90f79b04829712dae49ac047
Chaitanya Bandiwdekar <67865461+ChaitanyaBandiwdekar@users.noreply.github.com>
2022-11-28 18:45:59 +0530

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -11,6 +11,6 @@ public class DockerHelloWorld {
 	@GetMapping
 	public String getName() {
 		
-		return "Pipeline has been setup successfully by 1914027 and 1914049";
+		return "Pipeline has been setup successfully by 1914006, 1914007, 1914029, 1914037";
 	}
 }
